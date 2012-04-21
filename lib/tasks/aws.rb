e7989dac3d16031287cba2252ed77fd4af1b721e
Fabio Lessa <flessa@gmail.com>
2012-04-21 15:56:26 +1000

    a bunch of cleanups and now start/stop of ci works

--- lib/tasks/aws.rb
+++ lib/tasks/aws.rb
@@ -3,52 +3,50 @@ require "json"
 require "aws-sdk"
 
 namespace :aws do
-  AWS.config(:access_key_id     => "0C4XBKRVDWJBJZ8D5282",
-             :secret_access_key => "VxWnFP/KY4pTqFZVCyM6GqYOhDDsrdzOJ39N7P1A")
+  begin
+    settings_file = File.expand_path("#{File.dirname(__FILE__)}/../../conf/settings.yaml")
+    SETTINGS = YAML::parse(open(settings_file)).transform
+  rescue
+    puts "Error loading settings. Make sure you provide a configuration file at #{settings_file}".red
+    exit
+  end
+
+  AWS.config(:access_key_id     => SETTINGS["aws_access_key"],
+             :secret_access_key => SETTINGS["aws_secret_access_key"])
   TEMPLATES_DIR = "#{File.dirname(__FILE__)}"
   BOOTSTRAP_FILE = "ci-bootstrap.tar.gz"
   STACK_NAME = "twitter-stream-ci"
 
   directory BUILD_DIR
 
-  desc "creates the project's infrastructure in the Amazon cloud"
-  task :provision => :upload_bootstrap_files do
-    template_body = contents("#{TEMPLATES_DIR}/ci-cloud_formation_template.erb")
-    boot_script = ERB.new(contents("#{TEMPLATES_DIR}/bootstrap.erb")).result(binding)
+  desc "creates the CI environment"
+  task :ci_start do
+    template_body = File.read("#{TEMPLATES_DIR}/ci-cloud-formation-template.erb")
+    boot_script = ERB.new(File.read("#{TEMPLATES_DIR}/bootstrap.erb")).result(binding)
 
-    puts "creating aws stack, this might take a while... ".white
-    cloud = cloud_formation
-    cloud.create_stack(STACK_NAME,
-                       "TemplateBody" => ERB.new(template_body).result(binding),
-                       "Parameters" => { "KeyName" => SETTINGS["aws_ssh_key_name"],
-                                         "Password" => "hotbubbles",
-                                         "PSK" => "W3lcom3%1"})
-    stack = nil
-    until stack
-      sleep 1
-      stack = find_stack(cloud)
-    end
-    puts "your servers have been provisioned successfully".white
-  end
-
-  {"admin_server" => "ManagementConsole", "test_server" => "TestServer"}.each do |file_name, key_name|
-    file "#{BUILD_DIR}/#{file_name}" => ["aws:settings", BUILD_DIR] do
-      cloud = cloud_formation
-      stack = find_stack(cloud)
-      fail "could not find stack... did you provision your aws resources?".red if stack.nil?
-      puts "discovering #{file_name} address...".white
-      address = stack["Outputs"].find { |output| output["OutputKey"] == key_name }["OutputValue"]
-      puts "#{file_name} address: #{address}".green
-      File.open("#{BUILD_DIR}/#{file_name}", "w") do |file|
-        file.write address
-      end
+    cloud_formation = AWS::CloudFormation.new
+    stack = cloud_formation.stacks[STACK_NAME]
+    unless stack.exists?
+      puts "creating aws stack, this might take a while... ".white
+      stack = cloud_formation.stacks.create(STACK_NAME,
+                                            ERB.new(template_body).result(binding),
+                                            :parameters => { "KeyName" => SETTINGS["aws_ssh_key_name"] })
+      sleep 1 until stack.status == "CREATE_COMPLETE"
+      puts "the CI environment has been provisioned successfully".white
+    else
+      puts "the CI environment exists already. Nothing to do"
     end
   end
 
-  desc "stops all instances and releases all Amazon resources"
-  task :shutdown => :settings do
-    cloud_formation.delete_stack STACK_NAME
-    puts "shutdown command successful".green
+  desc "stops the CI environment"
+  task :ci_stop do
+    stack = AWS::CloudFormation.new.stacks[STACK_NAME]
+    if stack.exists?
+      stack.delete
+      puts "shutdown command successful".green
+    else
+      puts "couldn't find stack. Nothing to do"
+    end
   end
 
   task :upload_bootstrap_files => [:package] do
@@ -59,7 +57,6 @@ namespace :aws do
       puts "creating S3 bucket".cyan
       bucket = s3.buckets.create(bucket_name)
     end
-    puts bucket.methods.sort
     puts "uploading bootstrap package...".cyan
     bucket.objects[BOOTSTRAP_FILE].write(File.read("#{BUILD_DIR}/#{BOOTSTRAP_FILE}"))
   end
@@ -69,22 +66,5 @@ namespace :aws do
     cp_r "puppet", "#{BUILD_DIR}/package/puppet"
     puts "packaging boostrap files in #{BOOTSTRAP_FILE}"
     %x[cd #{BUILD_DIR}/package; tar -zcf ../#{BOOTSTRAP_FILE} *]
-  end
-
-  task :settings do
-    SETTINGS = YAML::parse(open("conf/settings.yaml")).transform
-    BUCKET_NAME = "#{SETTINGS['aws_s3_bucket_name']}"
-  end
-
-  def find_stack(cloud)
-    cloud.describe_stacks.body["Stacks"].find do |stack|
-      stack["StackName"] == STACK_NAME && stack["StackStatus"] == "CREATE_COMPLETE"
-    end
-  end
-
-  def contents(file)
-    contents = ""
-    File.open(file) { |f| contents << f.read }
-    contents
   end
 end
