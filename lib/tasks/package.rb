c6cebf2054cfbaf9db51a94937eb6225ff552628
Nigel Fernandes <dev@nigel.in>
2012-05-09 22:31:57 +1000

    fixing typo :nigel

--- lib/tasks/package.rb
+++ lib/tasks/package.rb
@@ -2,7 +2,7 @@ namespace :package do
   task :puppet do
     mkdir_p "#{BUILD_DIR}/package"
     cp_r "puppet", "#{BUILD_DIR}/package/puppet"
-    cp("conf/settings.yaml", "#{BUILD_DIR}/package/puppet") unless (ENV["GO_SERVER_URL"] || ENV["SETTINGS_FILE"]
+    cp("conf/settings.yaml", "#{BUILD_DIR}/package/puppet") unless (ENV["GO_SERVER_URL"] || ENV["SETTINGS_FILE"])
     sh "cd #{BUILD_DIR}/package; tar -zcf ../#{BOOTSTRAP_FILE} *"
   end
 end
