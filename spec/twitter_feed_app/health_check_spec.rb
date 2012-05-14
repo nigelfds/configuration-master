b411a3609df027b09fd9764795cedcdc1aaaa32d
Clarence Bakirtzidis <clarenceb@gmail.com>
2012-05-14 15:18:33 +1000

    Check app health deployed on the correct server (not localhost on CI environment)

--- spec/twitter_feed_app/health_check_spec.rb
+++ spec/twitter_feed_app/health_check_spec.rb
@@ -3,8 +3,14 @@ require "nokogiri"
 require "excon"
 
 describe "Twitter Feed App Health Check" do
+  before :each do
+    @app_url = "http://#{File.open("build/app-url", "r").read.strip}:8080"
+    @app_url.should_not be_empty
+    puts "Checking health of app at: #{@app_url}"
+  end
+
   it "should successfully display twitter statuses for the default hash tag" do
-    response = Excon.get('http://localhost:8080')
+    response = Excon.get(@app_url)
     response.status.should == 200
 
     html = Nokogiri::HTML.parse(response.body)
