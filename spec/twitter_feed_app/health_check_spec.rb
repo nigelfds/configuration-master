440e494d9498cb89faaa486629e676e7ecd8eba0
Clarence Bakirtzidis <clarenceb@gmail.com>
2012-05-14 17:37:13 +1000

    Added retry with delay for app verify stage to allow for app warmup

--- spec/twitter_feed_app/health_check_spec.rb
+++ spec/twitter_feed_app/health_check_spec.rb
@@ -10,11 +10,30 @@ describe "Twitter Feed App Health Check" do
   end
 
   it "should successfully display twitter statuses for the default hash tag" do
-    response = Excon.get(@app_url)
-    response.status.should == 200
+    html = get_twitter_statuses(@app_url)
 
-    html = Nokogiri::HTML.parse(response.body)
     html.css('#hash_tag').text.should == "#aws"
     html.css('#tweets .tweet').length.should > 0
+  end
+
+  def get_twitter_statuses(url)
+    retries = 5
+    html = ""
+    while retries > 0 && html.empty?
+      begin
+        response = Excon.get(url)
+        if response.status == 200
+          html = Nokogiri::HTML.parse(response.body)
+        else
+          raise "Response status: #{response.status}, Retying..."
+        end
+      rescue => e
+        $stderr.puts "Failed to get Twitter statuses from #{url}: #{e.message}"
+        $stderr.puts "Retrying..."
+        sleep 10
+      end
+      retries -= 1
+    end
+    html
   end
 end
